b99ee6b63aed7c51082ad96e5b18f735f4c3488d
Saurav K <sauravkoduvalli@gmail.com>
2024-10-26 15:11:33 +0530

    about js added

---
+++ javascript.js
@@ -0,0 +1,42 @@
+/* What is Javascript? */
+
+/*
+    - A dynamic programming language primarily used for creating web pages.
+    - It is a scripting language, meaning it is not needed to be compiled before execution. Instead, It's interpreted directly by the browser.
+
+    Key features
+    ------------
+    1) Dynamic typing: varibales can store different data types without explicit declaration.
+    2) Prototypal inheritance: Objects inherits properties and methods from other objects, providing a flexible object-oriented paradigm.
+    3) First-class functions: functions can be treated as values, assigned to varibales, passed as arguments and returned from other functions.
+    4) Event-driven programming: JS allows to create event handlers that responds to user interactions, such as click, mouse movements and keypress.
+    5) Asynchronous programming: JS supports asynchronous operations enabling non-blocking execution and handling tasks like network requests and timeouts efficiently.
+
+    Advantages
+    ----------
+    1) Client-side scripting: It can manipulate the Document Object Model (DOM) to create dynamic and interactive web pages.
+    2) Server-side scripting: Node.js allows you to build server-side applications with JS, enabling full-stack development.
+    3) Cross-platform development: JS can be used to develop web, mobile, and desktop applications.
+    4) Large and active community: A vast community provides extensive support, libraries, and frameworks.
+    5) Easy to learn: JS has a relatively simple syntax and a gentle learning curve.
+
+    Disadvantages
+    ------------
+    1) Browser compatibility: Different browser may interpret JS code in different ways, leading to compatibility issues.
+    2) Single  threaded: JS is single threaded which can limit it's ability to handle complec tasks without blocking the user interface.
+    
+    Compilation Vs. Interpretation
+    ------------------------------
+    Compilation: 
+        - The process of converting  or trandlating the JS code (human readablec code) into machine code before execution.
+        - This process is typically faster but less flexible.
+    Interpretation:
+        - The process of executing the code line by line, without a prior compilation steps.
+        - This approach is more flexible but can be slower in execution.
+    
+    JavaScript's Compilation Process
+    --------------------------------
+    - While JS is primarily an interpreted language, modern JS engines use a technique called Just-In-Time (JIT) compilation to optimize performance. 
+    - JIT compilers analyze and optimize JavaScript code during runtime, converting it into machine code for faster execution.
+    - JS combines the flexibility of interpretation with the performance benefits of compilation, making it a powerful and versatile language for web development.
+*/
